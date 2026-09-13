afe5eaece8ba954ee204728269a7aec8e461a835
Gregory Livingston <123greglivin@gmail.com>
2026-09-13 10:02:37 -0500

    Load mobile safe area styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from "next";
 import "./globals.css";
 import "./model-parts.css";
+import "./mobile-safe-area.css";
 import RegisterServiceWorker from "@/components/RegisterServiceWorker";
 
 export const metadata: Metadata = {
